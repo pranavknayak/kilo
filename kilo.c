4b66ee46dfd9851ab335a87de6f86f0a88af9b68
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-07-20 10:11:49 +0530

    turn off cursor for duration of refresh

--- kilo.c
+++ kilo.c
@@ -140,13 +140,14 @@ void editorDrawRows(struct abuf *ab) {
 
 void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
-
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
   abAppend(&ab, "\x1b[H", 3);
   write(STDOUT_FILENO, ab.b, ab.len);
+  abAppend(&ab, "\x1b[?25h", 6);
   abFree(&ab);
 }
 
