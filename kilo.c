d03fe12750892fc09e222d3b4923f28cb604a33b
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-07-20 10:22:16 +0530

    add welcome message

--- kilo.c
+++ kilo.c
@@ -10,6 +10,7 @@
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k)&0x1f)
+#define KILO_VERSION "0.0.1"
 
 /*** data ***/
 struct termios orig_termios;
@@ -130,7 +131,17 @@ void abFree(struct abuf *ab) { free(ab->b); }
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+                                "Kilo Editor -- Version %s", KILO_VERSION);
+      if (welcomelen > E.screencols)
+        welcomelen = E.screencols;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+
     abAppend(ab, "\x1b[K", 3);
 
     if (y < E.screenrows - 1) {
