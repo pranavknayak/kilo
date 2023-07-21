ee85b7a0a8873886d429084526daf22f5c7eb971
Pranav K Nayak <es20btech11035@iith.ac.in>
2023-07-21 12:17:19 +0530

    process pdDown and pgUp

--- kilo.c
+++ kilo.c
@@ -19,6 +19,8 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN,
 };
 
 struct termios orig_termios;
@@ -83,15 +85,28 @@ int editorReadKey() {
       return '\x1b';
 
     if (seq[0] == '[') {
-      switch (seq[1]) {
-      case 'A':
-        return ARROW_UP;
-      case 'B':
-        return ARROW_DOWN;
-      case 'C':
-        return ARROW_RIGHT;
-      case 'D':
-        return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+        case 'A':
+          return ARROW_UP;
+        case 'B':
+          return ARROW_DOWN;
+        case 'C':
+          return ARROW_RIGHT;
+        case 'D':
+          return ARROW_LEFT;
+        }
       }
     }
 
@@ -241,6 +256,15 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
+
+  case PAGE_DOWN:
+  case PAGE_UP: {
+    int times = E.screenrows;
+    while (times--) {
+      editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+    break;
+  }
 
   case ARROW_UP:
   case ARROW_DOWN:
